392cd60b8cb6f04e0f8b7a4ac9b1611e61243f53
Ed Skoviak <eskoviak@gmail.com>
2023-09-10 19:11:21 -0500

    initial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SetsComponent } from './sets/sets.component';
+import { CircuitComponent} from './circuit/circuit.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'sets', component: SetsComponent},
+  { path: 'circuit', component : CircuitComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
+
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SetsComponent } from './sets/sets.component';
+import { CircuitComponent } from './circuit/circuit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SetsComponent,
+    CircuitComponent
   ],
   imports: [
     BrowserModule,
